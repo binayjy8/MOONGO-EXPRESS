3bc352f6caae286e7baabe3ef1fa1b186547162c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-02 16:31:13 +0530

    backend

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 };
 
+let chat1 = new chat({
+    from : "Manash",
+    to : "Lipun",
+    msg : "when will your held",
+    created_at : new Date(),
+});
+
+chat1.save().then((res) => {
+    console.log(res);
+}).catch ((err) => {
+    console.log(err);
+});
+
 app.get("/", (req, res) => {
     res.send("root is working");
 });
