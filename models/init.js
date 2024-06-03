9a0b00b758cf89a9fd546a7a828a38f12e750b1b
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-03 15:19:00 +0530

    backend

--- models/init.js
+++ models/init.js
@@ -10,3 +10,16 @@ main()
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 };
+
+let chat1 = new chat({
+    from : "Manash",
+    to : "Lipun",
+    msg : "when will your held",
+    created_at : new Date(),
+});
+
+chat1.save().then((res) => {
+    console.log(res);
+}).catch ((err) => {
+    console.log(err);
+});
